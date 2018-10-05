464c7d011ac3e0608699c347ff25f1ee4544d973
maltebp <maltebp94@gmail.com>
2018-10-05 13:05:01 +0200

    Fixed bug with wrong balance in dices: all unit tests are now positive.

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -3,16 +3,16 @@ package terning;
 public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		double d1=Math.random()*6;     // 0-1
+		double d2=d1;                     // 0-5
+		//double d3=Math.round(d2);             // 0-5 integer
+		return (int) d2+1;                     // 1-6
 	}
 	
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
 		for (int i=1; i<n; i++) {
 			System.out.print(roll() + " ");
-		}		
+		}
 	}
 }
